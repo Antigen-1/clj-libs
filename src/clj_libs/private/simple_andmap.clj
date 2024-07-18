9afcff3a8df4207fb2276f7636b8da5d1afc1879
ZhangHao <zhanghao@antigen.top>
2024-07-18 18:52:51 +0800

    Update simple-andmap

--- src/clj_libs/private/simple_andmap.clj
+++ src/clj_libs/private/simple_andmap.clj
@@ -2,4 +2,9 @@
 
 (defn simple-andmap
   ([p s]
-   (reduce #(and %1 (let [r (p %2)] (and r (cons r %1)))) '() (reverse s))))
+   (loop [s s r '()]
+     (if (empty? s)
+       (reverse r)
+       (let [[fst & rst] s
+             res (p fst)]
+         (and res (recur rst (cons res r))))))))
