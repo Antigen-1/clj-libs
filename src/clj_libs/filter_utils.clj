bd306da8dad1477b0b23d8633869f4943eb9037e
ZhangHao <zhanghao@antigen.top>
2024-07-20 15:24:50 +0800

    Update filter-map

--- src/clj_libs/filter_utils.clj
+++ src/clj_libs/filter_utils.clj
@@ -1,5 +1,5 @@
 (ns clj-libs.filter-utils
-  (:use clj-libs.for-fold clj-libs.map-utils))
+  (:use clj-libs.for-fold clj-libs.map-utils clj-libs.zip-seqs))
 
 (defn filter-not
   "Like filter, except that elements are preserved when the predicate returns false or nil"
@@ -13,7 +13,7 @@
    (filter (fn [x] x) (map proc coll)))
   ([proc coll0 coll1 & colls]
    {:pre [(fn? proc) (andmap coll? (cons coll0 (cons coll1 colls)))]}
-   (filter (fn [x] x) (apply map proc (cons coll0 (cons coll1 colls))))))
+   (filter-map (fn [l] (apply proc l)) (zip (cons coll0 (cons coll1 colls))))))
 (defn filter-split
   "Split the collection according to the result of the predicate"
   [p s]
